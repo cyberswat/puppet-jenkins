5ad4ff61891a624a187fa63b5a06884f75209281
Kevin Bridges <kevin@cyberswat.com>
2012-02-27 11:43:49 -0500

    Initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,17 @@
+# Class: jenkins
+#
+# This module manages jenkins
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+# [Remember: No empty lines between comments and class definition]
+class jenkins {
+
+
+}
